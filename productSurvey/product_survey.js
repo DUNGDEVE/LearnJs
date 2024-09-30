fd12033da434b7eb427487967d4bc589e34dd0b1
DUNGDEVE <nguyenvietdung0506@gmail.com>
2024-09-30 11:49:28 -0400

    Completed product feedback survey form lab

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -8,9 +8,9 @@ function submitFeedback(){
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
-
+    const userExperince = document.getElementById('userExperince').value;
     // Gán giá trị vào các phần tử hiển thị thông tin
-    
+
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
     document.getElementById('userEmail').innerHTML = email;
@@ -18,6 +18,7 @@ function submitFeedback(){
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExpericeDisplay').innerHTML = userExperince;
     document.getElementById('userInfo').style.display = 'block';
     alert('Thank you for your valuable feedback')
 
